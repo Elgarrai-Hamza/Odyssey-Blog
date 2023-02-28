e4ab10131a30620c92875961fbf2bb212499430f
“Hamza <hamzaelgarrai.dev@gmail.com>
2023-02-28 15:43:40 +0100

    first commit

---
+++ About-Us-Page/main.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+//change nav style
+window.addEventListener('scroll', function () {
+  let header = document.querySelector('header');
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle('scrolling-active', windowPosition);
+ })
